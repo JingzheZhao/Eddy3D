82e835ff51e225acb635f4d1b23cb441c0e04278
Timur <tkdogan@cornell.edu>
2018-01-19 15:13:12 -0500

    testing different GetAssembly directory methods - none of them work on win10

--- VirtualWindTunnel/Utilities.cs
+++ VirtualWindTunnel/Utilities.cs
@@ -22,10 +22,28 @@ namespace WindTunnel
             }
         }
 
+        public static string GetDirectoryPath(this Assembly assembly)
+        {
+            string filePath = new Uri(assembly.CodeBase).LocalPath;
+            return Path.GetDirectoryName(filePath);
+        }
+
         static public string AssemblyDirectory
         {
             get
             {
+                var dir = AppDomain.CurrentDomain.BaseDirectory;
+                var localDir = Assembly.GetExecutingAssembly().GetDirectoryPath();
+                var dir1 = System.IO.Path.GetDirectoryName(new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+
+
+                var bla1 = Assembly.GetEntryAssembly();    //gives you the entrypoint assembly for the process.
+                var bla2 = Assembly.GetCallingAssembly();   // gives you the assembly from which the current method was called.
+                var bla3 = Assembly.GetExecutingAssembly(); // gives you the assembly in which the currently executing code is defined
+                var bla4 = Assembly.GetAssembly(typeof(OFBaseDomain));  // gives you the assembly in which the specified type is defined.
+                var loc = bla4.Location;
+                string path2 = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+
                 string codeBase = Assembly.GetExecutingAssembly().CodeBase;
                 UriBuilder uri = new UriBuilder(codeBase);
                 string path = Uri.UnescapeDataString(uri.Path);
