ffde52ce0d5c8370017d7ba08e6b43acfa76ed99
Timur <tkdogan@cornell.edu>
2018-04-22 13:57:24 -0400

    sdfsdfsfdsf

--- VirtualWindTunnel/Components/CompBlockMeshCyl.cs
+++ VirtualWindTunnel/Components/CompBlockMeshCyl.cs
@@ -100,15 +100,12 @@ namespace Eddy
 
             DA.GetData(1, ref baseWorkingDirectory);
 
-<<<<<<< HEAD
             BoundaryConditions BCond = null;
-=======
-            BoundaryConditions BCond = new BoundaryConditions();
->>>>>>> 82a1bfbb4d8c062f625a6d71f780aa1172359ca9
+
 
             DA.GetData(2, ref BCond);
 
-<<<<<<< HEAD
+
             GH_ObjectWrapper gobj = null;
             if (!DA.GetData(2, ref gobj)) { }
 
@@ -119,23 +116,7 @@ namespace Eddy
                 }
                 else { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Please pass a valid boundary condition object"); return; }
             
-=======
 
-
-            //List<BoundaryConditions> BCond = new List<BoundaryConditions>();
-
-            //List<GH_ObjectWrapper> gobj = new List<GH_ObjectWrapper>();
-            //if (!DA.GetDataList(2, gobj)) { }
-
-            //foreach (var obj in gobj)
-            //{
-            //    if ((obj.Value is BoundaryConditions))
-            //    {
-            //        BCond.Add((BoundaryConditions)obj.Value);
-            //    }
-            //    else { AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Please pass a valid boundary condition object"); return; }
-            //}
->>>>>>> 82a1bfbb4d8c062f625a6d71f780aa1172359ca9
 
             //int mode = 0;
             //int baseMesh = 0;
