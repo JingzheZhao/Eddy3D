e07f7648c6629aa76288a0df3472b0c333537b72
Patrick Kastner <patrick.kastner@gmail.com>
2018-05-30 00:29:00 -0400

    run.bat for ray, probes, and utci

--- EddyLib/StringTemplates/StringTemplates.cs
+++ EddyLib/StringTemplates/StringTemplates.cs
@@ -1540,6 +1540,9 @@ RAS
                 sb.AppendLine("call " + DOM.baseWorkingDirectory + i + "_run_sim.bat");
             }
             //sb.AppendLine("PAUSE");
+            sb.AppendLine("call " + DOM.baseWorkingDirectory + "run_ray.bat");
+            sb.AppendLine("call " + DOM.baseWorkingDirectory + "run_probes.bat");
+            sb.AppendLine("call " + DOM.baseWorkingDirectory + "run_utci.bat");
 
             return sb.ToString();
         }
