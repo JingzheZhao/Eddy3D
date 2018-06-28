5eb86c9b19982ebbde23abeee2bd83314c4ca30b
Patrick Kastner <patrick.kastner@gmail.com>
2018-06-28 19:12:40 +0200

    More warning messages

--- Eddy/Components/CompBlockMeshCyl.cs
+++ Eddy/Components/CompBlockMeshCyl.cs
@@ -179,7 +179,7 @@ namespace Eddy
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Your system does not have that much RAM available.");
             }
-            if (divisionsOuterCirc >= 50 && gradingPerim >= 10)
+            if (divisionsOuterCirc * gradingPerim >= 400)
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "A high number of outer divisions while at the same time using high perimeter grading might slow down mesh creation significantly.");
             }
