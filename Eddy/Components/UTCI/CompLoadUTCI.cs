b0d40c617870e0c1dc30a0e0bad5010dbffd3adb
Patrick Kastner <patrick.kastner@gmail.com>
2018-07-17 20:15:23 +0200

    Change category of UTCI reader component

--- Eddy/Components/UTCI/CompLoadUTCI.cs
+++ Eddy/Components/UTCI/CompLoadUTCI.cs
@@ -38,7 +38,7 @@ namespace Eddy
         /// new tabs/panels will automatically be created.
         /// </summary>
         public CompLoadUTCI()
-          : base("Load UTCI", "Load UTCI", "Misc", "Eddy", "Misc")
+          : base("Load UTCI", "Load UTCI", "Misc", "Eddy", "UTCI")
         {
         }
 
