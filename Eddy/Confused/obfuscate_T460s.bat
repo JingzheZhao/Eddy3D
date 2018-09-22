ff8ca5443f85fedbde355d37059623faa7ec5939
Patrick Kastner <patrick.kastner@gmail.com>
2018-09-22 18:26:12 -0400

    Obfuscate

--- Eddy/Confused/obfuscate_T460s.bat
+++ Eddy/Confused/obfuscate_T460s.bat
@@ -1,4 +1,7 @@
 "C:\Users\Patrick Kastner\Downloads\ConfuserEx_bin\Confuser.CLI.exe" "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\Confused\Eddy_T460s.crproj"
+del /s /q /f "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\bin\*.exe.config"
+del /s /q /f "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\bin\*.pdb"
+del /s /q /f "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\bin\*.xml"
 move /Y *.exe ..\bin
 move /Y *.dll ..\bin
 PAUSE
