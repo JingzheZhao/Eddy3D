c7a2c823e40b9fe53061330a9e7e54a7a14af1b5
Patrick Kastner <patrick.kastner@gmail.com>
2018-10-25 15:05:56 -0400

    Fix compilation

---
+++ Eddy/Compile/Installer_ESL.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /Qp "C:\Users\%USERNAME%\Documents\Github\Windtunnel\Eddy\Compile\installer_ESL.iss"
+PAUSE
--- Eddy/Compile/Installer_T460s.bat
+++ Eddy/Compile/Installer_T460s.bat
@@ -1,1 +1,2 @@
 "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /Qp "C:\Users\%USERNAME%\Documents\Github\Windtunnel\Eddy\Compile\installer.iss"
+PAUSE
