23e0567044343d006ccaebfeebfdaf0cd984f48b
Patrick Kastner <patrick.kastner@gmail.com>
2019-10-28 15:23:54 -0400

    dotfuscator

--- Eddy/Compile/obfuscate_ESL.bat
+++
@@ -1,8 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "C:\Users\pkastner\Documents\GitHub\WindTunnel\Eddy.sln"
-"C:\Users\pkastner\Documents\ConfuserEx_bin\Confuser.CLI.exe" "C:\Users\pkastner\Documents\GitHub\WindTunnel\Eddy\Compile\Eddy_ESL.crproj"
-del /s /q /f "C:\Users\pkastner\Documents\GitHub\WindTunnel\Eddy\bin\*.exe.config"
-del /s /q /f "C:\Users\pkastner\Documents\GitHub\WindTunnel\Eddy\bin\*.pdb"
-del /s /q /f "C:\Users\pkastner\Documents\GitHub\WindTunnel\Eddy\bin\*.xml"
-move /Y *.exe ..\bin
-move /Y *.dll ..\bin
-PAUSE
--- Eddy/Compile/obfuscate_T460s.bat
+++
@@ -1,8 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy.sln"
-"C:\Users\Patrick Kastner\Downloads\ConfuserEx_bin\Confuser.CLI.exe" "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\Compile\Eddy_T460s.crproj"
-del /s /q /f "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\bin\*.exe.config"
-del /s /q /f "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\bin\*.pdb"
-del /s /q /f "C:\Users\Patrick Kastner\Documents\GitHub\WindTunnel\Eddy\bin\*.xml"
-move /Y *.exe ..\bin
-move /Y *.dll ..\bin
-PAUSE
