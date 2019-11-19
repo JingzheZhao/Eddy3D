63386dd4e16457da0c06a980d6d2eae1b06b69e2
Timur <tkdogan@cornell.edu>
2019-11-19 18:06:24 -0500

    upping version

--- EddyLib/EddyVersion.cs
+++ EddyLib/EddyVersion.cs
@@ -8,7 +8,7 @@ namespace EddyLib
 {
     public static class EddyVersion
     {
-        public const string ProductVersion = "0.3.1.0";
+        public const string ProductVersion = "0.3.2.0";
         public const string Name = "Eddy3D";
 
         public static string toString()
