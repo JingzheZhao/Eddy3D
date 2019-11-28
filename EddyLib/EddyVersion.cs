376ccd60261b775790a979363fd914d60a59623a
Patrick Kastner <patrick.kastner@gmail.com>
2019-11-27 21:18:11 -0500

    V0.3.5.0

--- EddyLib/EddyVersion.cs
+++ EddyLib/EddyVersion.cs
@@ -8,7 +8,7 @@ namespace EddyLib
 {
     public static class EddyVersion
     {
-        public const string ProductVersion = "0.3.4.0";
+        public const string ProductVersion = "0.3.5.0";
         public const string Name = "Eddy3D";
 
         public static string toString()
