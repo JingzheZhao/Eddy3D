4956482cdb93fec2cc8ced41e7287ef4d8d622d3
Patrick Kastner <patrick.kastner@gmail.com>
2019-12-04 12:16:47 -0500

    Update checkMesh command for OF5

--- EddyLib/StringTemplates/BatchFiles.cs
+++ EddyLib/StringTemplates/BatchFiles.cs
@@ -15,6 +15,7 @@ namespace EddyLib.StrTemp
         //Run commands as list
 
         private static readonly List<string> RCCheckMeshSingleCPU = new List<string> {
+       // "checkMesh -allGeometry -allTopology -writeAllFields -writeSets vtk",  // Not supported in OpenFOAM 5 yet
         "checkMesh -allGeometry -allTopology -writeAllFields -writeSets vtk",
         "foamToVTK -faceSet highAspectRatioCells -ascii",
         "foamToVTK -faceSet nonOrthoFaces -ascii",
