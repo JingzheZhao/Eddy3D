47736550522d1a3f464c68874a05aeb5514b5995
Patrick Kastner <patrick.kastner@gmail.com>
2020-09-21 17:00:14 -0400

    Use new method to cd into case directory

--- EddyLib/Strings/BatchFiles.cs
+++ EddyLib/Strings/BatchFiles.cs
@@ -796,13 +796,11 @@ REM   --help              Display this help screen.");
             }
 
             StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(string.Format(@"setlocal"));
             sb.AppendLine(string.Format(@"call """ + installationPath + @"\setvars.bat"""));
             sb.AppendLine(@"set PATH=%HOME%\msys64\usr\bin;%PATH%");
-            if (notOnC)
-            {
-                sb.AppendLine(caseDir[0] + ":");
-            }
-            sb.AppendLine("cd " + "\"" + caseDir + "\"");
+            sb.AppendLine(@"cd /d %~dp0");
 
             if (logging)
             {
