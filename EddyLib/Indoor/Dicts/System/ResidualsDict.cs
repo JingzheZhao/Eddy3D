c9e5a8938f79ec195ee940273c44224dc73114c9
Zoe De Simone <56321398+zoedesimone@users.noreply.github.com>
2021-02-24 17:21:09 -0500

    Updated ResidualsDict to dictionaries

--- EddyLib/Indoor/Dicts/System/ResidualsDict.cs
+++ EddyLib/Indoor/Dicts/System/ResidualsDict.cs
@@ -9,6 +9,9 @@ namespace EddyLib.Indoor.Dicts
 {
     public class ResidualsDict : GenericDict
     {
+
+        public List<String> InternalDict = new List<string>();
+
         public ResidualsDict()
         {
             this.DictionaryName = "residuals";
@@ -17,15 +20,42 @@ namespace EddyLib.Indoor.Dicts
             this.FC = FieldClass.dictionary;
             this.Header = GetHeader(this);
 
-            this.FullDictString = @"type            residuals;
-libs            (""libutilityFunctionObjects.so"");
+            this.InternalDict.Add(CppMapSerializerDyn.Serialize(GetWriteControlDict()));
 
-writeControl timeStep;
-writeInterval   1;
+           string[] parts = {
+         //      this.Header, "\n",
+         String.Join("\n", this.InternalDict.ToArray())
+            };
 
-fields (    p_rgh   U  h k omega AoA   );
+            this.FullDictString = parts.Aggregate((partialPhrase, word) => $"{partialPhrase} {word}");
+        }
 
-// ************************************************************************* //";
+        private static Dictionary<string, dynamic> GetWriteControlDict()
+        {
+
+            Dictionary<string, dynamic> InternalDict = new Dictionary<string, dynamic>();
+
+            //Dictionary<string, dynamic> FunctionObjectlDict = new Dictionary<string, dynamic>();
+
+            InternalDict.Add("type", "residuals");
+            InternalDict.Add("libs", " (\"libutilityFunctionObjects.so\") ");
+
+            InternalDict.Add("writeControl", "timeStep");
+            InternalDict.Add("writeInterval", "1");
+            InternalDict.Add("fields", "(    p_rgh   U  h k omega AoA   )");
+
+            return InternalDict;
         }
     }
 }
+
+//            @"type            residuals;
+//libs            (""libutilityFunctionObjects.so"");
+
+//writeControl timeStep;
+//writeInterval   1;
+
+//fields (    p_rgh   U  h k omega AoA   );
+
+//// ************************************************************************* //";
+//        }
