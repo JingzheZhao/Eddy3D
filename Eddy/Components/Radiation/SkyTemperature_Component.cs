ab51a9b8de1ed7240ea84340df349267937346b8
Timur Dogan <tkdogan@cornell.edu>
2021-03-11 11:59:28 -0500

    ...

--- Eddy/Components/Radiation/SkyTemperature_Component.cs
+++ Eddy/Components/Radiation/SkyTemperature_Component.cs
@@ -30,7 +30,7 @@ namespace Eddy.Components.Radiation
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddNumberParameter("Weather", "W", "Weather filepath", GH_ParamAccess.item);
+            pManager.AddNumberParameter("SkyT", "ST", "Sky temperature", GH_ParamAccess.item);
         }
 
         /// <summary>
