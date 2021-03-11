b300d570a3a48daaee61b0b5d23988815508bf0a
Timur Dogan <tkdogan@cornell.edu>
2021-03-11 11:52:11 -0500

    adding a component for sky temperature

--- EddyLib/Radiation/RPolygon.cs
+++ EddyLib/Radiation/RPolygon.cs
@@ -10,17 +10,15 @@ namespace EddyLib.Radiation
     public class RPolygon
     {
         public double rin = 0.0;
-        public double rout = 0.0;
-        
+        public double rout = 0.0;     
         public double refl = 0.0;
 
         public Vector3d n;
         public Point3d cen;
-        public double area = 0.0;
         public Mesh m;
+        public double area = 0.0;
 
         public string matName;
-
         public RSurface parent;
      }
 
