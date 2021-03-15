bcbd34bcec6e38b2ad020d4b37e18a7d3031c918
Patrick Kastner <patrick.kastner@gmail.com>
2021-03-15 17:46:16 -0400

    fixes

--- Build/Installer/BuildInstaller.bat
+++ Build/Installer/BuildInstaller.bat
@@ -4,5 +4,6 @@ del /s /q /f "..\Dotfuscated\*.exe.config"
 del /s /q /f "..\Dotfuscated\*.pdb"
 del /s /q /f "..\Dotfuscated\*.xml"
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Qp "C:\Users\%USERNAME%\Documents\GitHub\Eddy3d\Build\Installer\Installer.iss"
-robocopy . "X:\Patrick\Eddy\Installer" /E /XD Strings /XD GHLink /XF BuildInstaller.bat /XF Installer.iss
-PAUSE
+robocopy . "X:\Patrick\Eddy\Installer" /E /XD Strings /XD GHLink /XF BuildInstaller.bat /XF Installer.iss /XF MountGDrive.bat /XF UnmountGDrive.bat
+call UnmountGDrive.bat
+timeout 500
