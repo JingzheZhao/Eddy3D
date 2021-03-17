05aba1c8ac50651be5d014fa5c3b8f37583e5b4e
Timur Dogan <tkdogan@cornell.edu>
2021-03-17 10:24:13 -0400

    updated example

---
+++ EddyLib/Radiation/MRT_Simulation_Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EddyLib.Radiation
+{
+    [DataContract]
+    public class MRT_Simulation_Settings
+    {
+        public MRT_Simulation_Settings() { }
+
+         
+        [DataMember]
+        public double CummulativeViewFactorCutoff { get; set; } = 0.1;
+
+        [DataMember]
+        public bool ComputeReflectionsAndDiffuseRadiation { get; set; } = true;
+
+    }
+}
