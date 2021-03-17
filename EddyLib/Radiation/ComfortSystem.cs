4489fd0976ea15eaa328e2a471c841ce518150cc
Timur Dogan <tkdogan@cornell.edu>
2021-03-16 22:19:38 -0400

    clamping extreme UTCI inputs

--- EddyLib/Radiation/ComfortSystem.cs
+++ EddyLib/Radiation/ComfortSystem.cs
@@ -94,7 +94,29 @@ namespace EddyLib.Radiation
 
                 for (int h = 0; h < 8760; h++)
                 {
-                    double utci = UTCI.CalcUTCI(this.Weather.DryBulbTemp[h], this.Weather.RelativeHumidity[h], windspeed[h], mrt[h]);
+
+
+                    // Check for extreme MRTs
+
+                    double resultingMRT = mrt[h];
+
+                    if (resultingMRT < this.Weather.DryBulbTemp[h] - 30) { resultingMRT = this.Weather.DryBulbTemp[h] - 30;  }
+                    if (resultingMRT > this.Weather.DryBulbTemp[h] + 70) { resultingMRT = this.Weather.DryBulbTemp[h] + 70;  }
+
+                    // Check for extreme Windspeeds
+
+                    double resultingWindSpeedforUTCI = windspeed[h];
+
+                    if (resultingWindSpeedforUTCI > 17) { resultingWindSpeedforUTCI = 17;   }
+                    if (resultingWindSpeedforUTCI < 0.5) { resultingWindSpeedforUTCI = 0.5; }
+
+                    // lift to 10 m height as required
+
+                    var resultingWindSpeedforUTCI_At10 = UTCI.At10Meters(resultingWindSpeedforUTCI, probe.Point.Value.Z);
+
+
+
+                    double utci = UTCI.CalcUTCI(this.Weather.DryBulbTemp[h], this.Weather.RelativeHumidity[h], resultingWindSpeedforUTCI, resultingMRT);
                     probe.UTCI[h] = (float) utci;
                 }
 
