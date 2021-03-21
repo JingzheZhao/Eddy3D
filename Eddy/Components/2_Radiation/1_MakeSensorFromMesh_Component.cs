07da9d1f6de9a880a9a73c0166f84e673e958a32
Timur Dogan <tkdogan@cornell.edu>
2021-03-21 12:35:12 -0400

    cleaning up example file, adding icons to Github, sensors from mesh

---
+++ Eddy/Components/2_Radiation/1_MakeSensorFromMesh_Component.cs
@@ -0,0 +1,80 @@
+﻿using Eddy.Properties;
+using EddyLib;
+using EddyLib.Radiation;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+
+namespace Eddy.Components.Radiation
+{
+    public class MakeRadiationSensorFromMesh_Component : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MakeRadiationSensor_Component class.
+        /// </summary>
+        public MakeRadiationSensorFromMesh_Component()
+          : base("Sensor", "Sen", "Simulation Sensor" + EddyVersion.toString(), EddyVersion.Name, "2 | Radiation")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddMeshParameter("Mesh", "M", "Grid mesh. Each face is turned into a sensor.", GH_ParamAccess.item);
+         }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Sensors", "Sen", "Radiation Simulation Sensors", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+           
+            Mesh m = null;
+
+           
+            DA.GetData(0, ref m);
+
+            List<EddyProbe> probes = new List<EddyProbe>();
+
+            if (m != null) {
+
+                probes.AddRange(EddyProbe.Mesh2Probes(m));
+            }
+
+            DA.SetDataList(0, probes);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return Resources.Eddy_SensorFromMesh;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{998A5593-8CD0-4E97-A466-EC71AE6C4FAE}"); }
+        }
+    }
+}
--- EddyLib/Geometry/EddyProbe.cs
+++ EddyLib/Geometry/EddyProbe.cs
@@ -29,8 +29,68 @@ namespace EddyLib.Radiation
       
         public Vector3d Normal { get; set; }
 
-         
+         public double Area { get; set; }
         public Mesh PreviewGeo { get; set; }
 
+
+
+
+        public static List<EddyProbe> Mesh2Probes(Mesh _ms)
+        {
+            List<EddyProbe> probes = new List<EddyProbe>();
+            if (_ms == null) return probes;
+
+            _ms.FaceNormals.ComputeFaceNormals();
+
+
+
+            for (int i = 0; i < _ms.Faces.Count; ++i)
+            {
+                EddyProbe pg = new EddyProbe();
+                pg.Point =  (_ms.Faces.GetFaceCenter(i));
+                pg.Normal = (_ms.FaceNormals[i]);
+                pg.Normal.Unitize();
+
+                probes.Add(pg);
+
+
+                if (_ms.Faces[i].IsQuad)
+                {
+                    Point3d v0 = new Point3d(_ms.Vertices[_ms.Faces[i].A]);
+                    Point3d v1 = new Point3d(_ms.Vertices[_ms.Faces[i].B]);
+                    Point3d v2 = new Point3d(_ms.Vertices[_ms.Faces[i].C]);
+                    Point3d v3 = new Point3d(_ms.Vertices[_ms.Faces[i].D]);
+
+                    Vector3d n1 = Vector3d.CrossProduct(v1 - v0, v2 - v0);
+                    Vector3d n2 = Vector3d.CrossProduct(v2 - v0, v3 - v0);
+
+                    pg.Area =  (n1.Length * 0.5 + n2.Length * 0.5);
+
+                    pg.PreviewGeo = (new Mesh());
+                    pg.PreviewGeo.Vertices.Add(v0);
+                    pg.PreviewGeo.Vertices.Add(v1);
+                    pg.PreviewGeo.Vertices.Add(v2);
+                    pg.PreviewGeo.Vertices.Add(v3);
+                    pg.PreviewGeo.Faces.AddFace(0, 1, 2, 3);
+                }
+                else
+                {
+                    Point3d v0 = new Point3d(_ms.Vertices[_ms.Faces[i].A]);
+                    Point3d v1 = new Point3d(_ms.Vertices[_ms.Faces[i].B]);
+                    Point3d v2 = new Point3d(_ms.Vertices[_ms.Faces[i].C]);
+
+                    Vector3d n1 = Vector3d.CrossProduct(v1 - v0, v2 - v0);
+
+                    pg.Area =  (n1.Length * 0.5);
+
+                    pg.PreviewGeo =  (new Mesh());
+                    pg.PreviewGeo.Vertices.Add(v0);
+                    pg.PreviewGeo.Vertices.Add(v1);
+                    pg.PreviewGeo.Vertices.Add(v2);
+                    pg.PreviewGeo.Faces.AddFace(0, 1, 2);
+                }
+            }
+            return probes;
+        }
     }
 }
