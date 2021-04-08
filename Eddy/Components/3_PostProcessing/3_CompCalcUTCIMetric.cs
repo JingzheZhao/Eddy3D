cdd635e4526920f5f373842c8ca0da6bf9d49128
Timur Dogan <tkdogan@cornell.edu>
2021-04-08 15:36:09 -0400

    ...

--- Eddy/Components/3_PostProcessing/3_CompCalcUTCIMetric.cs
+++ Eddy/Components/3_PostProcessing/3_CompCalcUTCIMetric.cs
@@ -16,12 +16,7 @@ namespace Eddy
         //    get { return GH_Exposure.hidden; }
         //}
 
-        // exposure
-        //public override GH_Exposure Exposure
-        //{
-        //    get { return GH_Exposure.hidden; }
-        //}
-
+  
         /// <summary>
         /// Each implementation of GH_Component must provide a public constructor without any
         /// arguments. Category represents the Tab in which the component will appear, Subcategory
