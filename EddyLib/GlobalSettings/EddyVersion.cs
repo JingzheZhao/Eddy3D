46e51713860c7517038d9eaf4bf89ccaed8973c0
Timur Dogan <tkdogan@cornell.edu>
2021-04-20 14:54:05 -0400

    upping version to 4.00.5

--- EddyLib/GlobalSettings/EddyVersion.cs
+++ EddyLib/GlobalSettings/EddyVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class EddyVersion
     {
-        public const string ProductVersion = "0.4.0.3";
+        public const string ProductVersion = "0.4.0.5";
 
         public const string Name = "Eddy3D";
 
