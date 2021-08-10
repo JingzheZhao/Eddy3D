31be9c36d5e680ba6486500f00da4cf15b299b07
Patrick Kastner <patrick.kastner@gmail.com>
2021-08-10 09:47:49 -0400

    Attempt to merge

--- EddyLib/Radiation/Old/TwoPhaseDDS.cs
+++ EddyLib/Radiation/Old/TwoPhaseDDS.cs
@@ -33,7 +33,7 @@ namespace EddyLib.Radiation
 
         private string TwoPhaseDDSFolder = @"\TwoPhaseDDS\";
 
-        public TwoPhaseDDS(string baseWorkingDir, Mesh BuildingGeometry, List<Point3d> probes, Weather weather, bool recalc, string RadianceDir = @"C:\Radiance")
+        public TwoPhaseDDS(string baseWorkingDir, Mesh BuildingGeometry, List<Point3d> probes, Weather weather, bool recalc, string RadianceDir = @"C:\Program Files\Radiance")
 
         {
             string csvDDS = Path.Combine(baseWorkingDir + fileName + del + fileNameCSVExtension);
@@ -89,7 +89,7 @@ namespace EddyLib.Radiation
             }
         }
 
-        protected void RunDDS(string baseWorkingDir, string subfolder, Mesh BuildingGeometry, List<Point3d> probes, Weather weather, bool run, string RadianceDir = @"C:\Radiance")
+        protected void RunDDS(string baseWorkingDir, string subfolder, Mesh BuildingGeometry, List<Point3d> probes, Weather weather, bool run, string RadianceDir = @"C:\Program Files\Radiance")
         {
             var numberOfProbes = probes.Count;
 
