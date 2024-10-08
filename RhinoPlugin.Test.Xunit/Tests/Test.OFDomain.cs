7b53834687c9fd5a1d53c69d27c04abf4001b691
Patrick Kastner <patrick.kastner@gmail.com>
2024-10-08 00:34:39 -0400

    Cleaned up tests

--- RhinoPlugin.Test.Xunit/Tests/Test.OFDomain.cs
+++ RhinoPlugin.Test.Xunit/Tests/Test.OFDomain.cs
@@ -12,6 +12,7 @@ namespace RhinoPlugin.Tests.Xunit
 {
     [Collection("Rhino Collection")]
     public class OFDomainTests
+
     {
         public List<double[]> CylDomainVertices
         {
@@ -1094,24 +1095,7 @@ new double[] { 121.4941, 110.3447, 80 }
         {
             // Arrange
 
-            var point0 = new Rhino.Geometry.Point3d(0, 0, 0);
-            var point1 = new Rhino.Geometry.Point3d(20, 0, 0);
-            var point2 = new Rhino.Geometry.Point3d(0, 20, 0);
-            var point3 = new Rhino.Geometry.Point3d(20, 20, 0);
-            var point4 = new Rhino.Geometry.Point3d(0, 0, 40);
-            var point5 = new Rhino.Geometry.Point3d(20, 0, 40);
-            var point6 = new Rhino.Geometry.Point3d(0, 20, 40);
-            var point7 = new Rhino.Geometry.Point3d(20, 20, 40);
-            Rhino.Geometry.Box box1 = new Rhino.Geometry.Box(Rhino.Geometry.Plane.WorldXY, new List<Rhino.Geometry.Point3d>() { point0, point1, point2, point3, point4, point5, point6, point7 });
-            Rhino.Geometry.MeshingParameters mp = new Rhino.Geometry.MeshingParameters();
-            var m = Mesh.CreateFromBrep(box1.ToBrep(), mp);
-
-            Rhino.Geometry.Mesh mm = new Rhino.Geometry.Mesh();
-
-            foreach (Rhino.Geometry.Mesh im in m)
-            {
-                mm.Append(im);
-            }
+            var mm = Setup.SetUpBuildingMesh();
 
             // var windDirList = new List<int>() { 0, 45, 90, 135, 180, 225, 270, 315 };
             BC bcond = new ABL(0, 5, 10, 1, 0);
@@ -1157,24 +1141,7 @@ new double[] { 121.4941, 110.3447, 80 }
         {
             // Arrange
 
-            var point0 = new Rhino.Geometry.Point3d(0, 0, 0);
-            var point1 = new Rhino.Geometry.Point3d(20, 0, 0);
-            var point2 = new Rhino.Geometry.Point3d(0, 20, 0);
-            var point3 = new Rhino.Geometry.Point3d(20, 20, 0);
-            var point4 = new Rhino.Geometry.Point3d(0, 0, 40);
-            var point5 = new Rhino.Geometry.Point3d(20, 0, 40);
-            var point6 = new Rhino.Geometry.Point3d(0, 20, 40);
-            var point7 = new Rhino.Geometry.Point3d(20, 20, 40);
-            Rhino.Geometry.Box box1 = new Rhino.Geometry.Box(Rhino.Geometry.Plane.WorldXY, new List<Rhino.Geometry.Point3d>() { point0, point1, point2, point3, point4, point5, point6, point7 });
-            Rhino.Geometry.MeshingParameters mp = new Rhino.Geometry.MeshingParameters();
-            var m = Mesh.CreateFromBrep(box1.ToBrep(), mp);
-
-            Rhino.Geometry.Mesh mm = new Rhino.Geometry.Mesh();
-
-            foreach (Rhino.Geometry.Mesh im in m)
-            {
-                mm.Append(im);
-            }
+            var mm = Setup.SetUpBuildingMesh();
 
             var windDirList = new List<int>() { 0, 22, 45, 90, 135, 180, 225, 270, 315 };
 
@@ -1190,24 +1157,7 @@ new double[] { 121.4941, 110.3447, 80 }
         {
             // Arrange
 
-            var point0 = new Rhino.Geometry.Point3d(0, 0, 0);
-            var point1 = new Rhino.Geometry.Point3d(20, 0, 0);
-            var point2 = new Rhino.Geometry.Point3d(0, 20, 0);
-            var point3 = new Rhino.Geometry.Point3d(20, 20, 0);
-            var point4 = new Rhino.Geometry.Point3d(0, 0, 40);
-            var point5 = new Rhino.Geometry.Point3d(20, 0, 40);
-            var point6 = new Rhino.Geometry.Point3d(0, 20, 40);
-            var point7 = new Rhino.Geometry.Point3d(20, 20, 40);
-            Rhino.Geometry.Box box1 = new Rhino.Geometry.Box(Rhino.Geometry.Plane.WorldXY, new List<Rhino.Geometry.Point3d>() { point0, point1, point2, point3, point4, point5, point6, point7 });
-            Rhino.Geometry.MeshingParameters mp = new Rhino.Geometry.MeshingParameters();
-            var m = Mesh.CreateFromBrep(box1.ToBrep(), mp);
-
-            Rhino.Geometry.Mesh mm = new Rhino.Geometry.Mesh();
-
-            foreach (Rhino.Geometry.Mesh im in m)
-            {
-                mm.Append(im);
-            }
+            var mm = Setup.SetUpBuildingMesh();
 
             var windDirList = new List<int>() { 0, 22, 45, 90, 135, 180, 225, 270, 315 };
 
@@ -1229,24 +1179,7 @@ new double[] { 121.4941, 110.3447, 80 }
         {
             // Arrange
 
-            var point0 = new Rhino.Geometry.Point3d(0, 0, 0);
-            var point1 = new Rhino.Geometry.Point3d(20, 0, 0);
-            var point2 = new Rhino.Geometry.Point3d(0, 20, 0);
-            var point3 = new Rhino.Geometry.Point3d(20, 20, 0);
-            var point4 = new Rhino.Geometry.Point3d(0, 0, 40);
-            var point5 = new Rhino.Geometry.Point3d(20, 0, 40);
-            var point6 = new Rhino.Geometry.Point3d(0, 20, 40);
-            var point7 = new Rhino.Geometry.Point3d(20, 20, 40);
-            Rhino.Geometry.Box box1 = new Rhino.Geometry.Box(Rhino.Geometry.Plane.WorldXY, new List<Rhino.Geometry.Point3d>() { point0, point1, point2, point3, point4, point5, point6, point7 });
-            Rhino.Geometry.MeshingParameters mp = new Rhino.Geometry.MeshingParameters();
-            var m = Mesh.CreateFromBrep(box1.ToBrep(), mp);
-
-            Rhino.Geometry.Mesh mm = new Rhino.Geometry.Mesh();
-
-            foreach (Rhino.Geometry.Mesh im in m)
-            {
-                mm.Append(im);
-            }
+            var mm = Setup.SetUpBuildingMesh();
 
             BC bcond = new ABL(330, 5, 10, 1, 0);
 
