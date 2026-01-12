41d8f4260bfaef948c30ed91035d01abe3dbfdc7
Ilker Karadag <decodeanddesign@gmail.com>
2026-01-12 11:18:57 -0500

    Template updated

--- Eddy/Properties/Resources.Designer.cs
+++ Eddy/Properties/Resources.Designer.cs
@@ -786,5 +786,14 @@ namespace Eddy.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Eddy3D_install {
+            get {
+                object obj = ResourceManager.GetObject("Eddy3D_install", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
