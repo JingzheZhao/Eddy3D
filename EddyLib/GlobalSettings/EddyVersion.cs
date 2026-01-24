668e6382694f7007df7f0e1f08e9493305fb1b0f
Ilker Karadag <decodeanddesign@gmail.com>
2026-01-23 20:59:22 -0500

    Update EddyVersion.cs

--- EddyLib/GlobalSettings/EddyVersion.cs
+++ EddyLib/GlobalSettings/EddyVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class EddyVersion
     {
-        public const string ProductVersion = "0.5.4.815";
+        public const string ProductVersion = "0.5.5.815";
         public const string Name = "Eddy3D";
 
         public static string toString()
