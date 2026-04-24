b1c8143f2ba84cc4626f764267979d151b09c480
Patrick Kastner <1919773+kastnerp@users.noreply.github.com>
2026-04-24 17:32:15 -0400

    Update EddyVersion.cs

--- EddyLib/GlobalSettings/EddyVersion.cs
+++ EddyLib/GlobalSettings/EddyVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class EddyVersion
     {
-        public const string ProductVersion = "0.7.0.827";
+        public const string ProductVersion = "0.7.1.827";
         public const string Name = "Eddy3D";
 
         public static string toString()
